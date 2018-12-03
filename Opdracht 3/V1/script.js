12db93a2eed4ef9ed74009e72027c2998c2fc1d8
Marc <marc.hessing@gmail.com>
2018-12-03 10:33:08 +0100

    update

--- Opdracht 3/V1/script.js
+++ Opdracht 3/V1/script.js
@@ -6,42 +6,61 @@ var images = document.querySelectorAll('.frame .image-box'); // Telt het aantal 
 var counter = 0;
 
 
+function imageSelector() {
+	console.log('counter:', counter);
 
-// Hier moet de counter komen
 
+	// Switch statement die de classes toevoegd aan .images 
+	// Hier moet nog een function voor geschreven worden.
+
+	switch (counter) {
+		console.log('hier sttat een switch');
+
+		case 0:
+			frame.classList.add('pos0');
+			//console.log("counter", counter);
+			break;
+		case 1:
+			frame.classList.add('pos1');
+			//console.log("counter", counter);
+			break;
+		case 2:
+			frame.classList.add('pos2');
+			//console.log("counter", counter);
+			break;
+		case 3:
+			frame.classList.add('pos3');
+			//console.log("counter", counter);
+			break;
+		case 4:
+			frame.classList.add('pos4');
+			//console.log("counter", counter);
+			break;
+		default:
+			//console.log("default is actief");
+
+
+	};
+};
+
+
+// On-click actions +1 en -1 
 
 next.addEventListener('click', function() {
-	counter +1;
-})
+	counter = counter + 1;
+	//console.log('counter: ', counter);
+});
 
-console.log('counter: ', counter);
-
-// Switch statement die de classes toevoegd aan .images 
-// Hier moet nog een function voor geschreven worden.
-
-switch (counter) {
-	case 0:
-		frame.classList.add('pos0');
-		console.log("counter", counter);
-		break;
-	case 1:
-		frame.classList.add('pos1');
-		console.log("counter", counter);
-		break;
-	case 2:
-		frame.classList.add('pos2');
-		console.log("counter", counter);
-		break;
-	case 3:
-		frame.classList.add('pos3');
-		console.log("counter", counter);
-		break;
-	case 4:
-		frame.classList.add('pos4');
-		console.log("counter", counter);
-		break;
-	default:
-		console.log("default is actief");
+prev.addEventListener('click', function() {
+	counter = counter - 1;
+	//console.log('counter: ', counter);
+});
 
 
-} 
+
+imageSelector();
+
+
+
+
+
