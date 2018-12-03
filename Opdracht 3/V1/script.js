1a3cbcc6f1964dcf8c0673c3b9a367979bb3f9c7
Marc <marc.hessing@gmail.com>
2018-12-03 17:08:45 +0100

    Opdracht 3 update

--- Opdracht 3/V1/script.js
+++ Opdracht 3/V1/script.js
@@ -11,7 +11,6 @@ var counter = 0;
 
 next.addEventListener('click', function() {
 	counter = counter + 1; //counter +=1
-	console.log("hoeveel zijn er ?", images.length)
 	if(counter > images.length-1){
 		counter = 0;
 	}
@@ -35,27 +34,69 @@ prev.addEventListener('click', function() {
 function imageSelector() {
 	clearclasses();
 	if (counter == 0) {
-		console.log('de counter staat op 0');
+		//console.log('de counter staat op 0');
 		frame.classList.add('pos0');
 	}
 	else if (counter == 1) {
-		console.log('de counter staat op 1');
+		//console.log('de counter staat op 1');
 		frame.classList.add('pos1');
 	}
 	else if (counter == 2) {
-		console.log('de counter staat op 2');
+		//console.log('de counter staat op 2');
 		frame.classList.add('pos2');
 	}
 	else if (counter == 3) {
-		console.log('de counter staat op 3');
+		//console.log('de counter staat op 3');
 		frame.classList.add('pos3');
 	}
 	else {
 		console.log('Er is hier geen afbeelding voor.')
 	}
-
-
 };
+
+
+// Hier komt de function voor de ARROw besturing
+
+document.onkeydown = function(event) {
+	console.log(event.keyCode);
+
+	if (counter == 0 && event.keyCode == 37) {
+		frame.classList.add('pos3');
+		console.log('0 naar 3');
+	}
+	else if (counter == 0 && event.keyCode == 39) {
+		frame.classList.add('pos1');
+		console.log('0 naar 1');
+	}
+
+	else if (counter == 1 && event.keyCode == 37) {
+		frame.classList.add('pos0');
+		console.log('1 naar 0');
+	}
+	else if (counter == 1 && event.keyCode == 39) {
+		frame.classList.add('pos2');
+		console.log('1 naar 2');
+	}
+
+	else if (counter == 2 && event.keyCode == 37) {
+		frame.classList.add('pos1');
+		console.log('2 naar 1');
+	}
+	else if (counter == 2 && event.keyCode == 39) {
+		frame.classList.add('pos3');
+		console.log('2 naar 3');
+	}
+
+	else if (counter == 3 && event.keyCode == 37) {
+		frame.classList.add('pos2');
+		console.log('3 naar 2');
+	}
+	else if (counter == 3 && event.keyCode == 39) {
+		frame.classList.add('pos0');
+		console.log('3 naar 0');
+	}
+}
+
 
 function clearclasses(){
 		frame.classList.remove('pos0');
